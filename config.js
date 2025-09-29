6f73cdcb6075dd65ce8111f87445cf32557ea437
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-09-29 18:17:39 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
-export const TELEGRAM_BOT_TOKEN = ''; // bot telegram token
+export const TELEGRAM_BOT_TOKEN = '8326661167:AAH685TaT4YNLPKR_O3vT19yQzV2Y7QBcHY'; // bot telegram token
 
 export const REDIRECT_BOT = "None" // a redirect bot when the bot is full
 
-export const OWNER_ID = "" // owner id
+export const OWNER_ID = "6587556123" // owner id
 
 export const LIMIT = 30; //number of max sessions for the tele bot
 
@@ -11,17 +11,17 @@ export const MODE = "Default"; // Your access key to run this bot
 
 export const PUB = true // Is it a private or public group
 
-export const OWNER_NAME = "Senku" // Your Dev username
+export const OWNER_NAME = "obito" // Your Dev username
 
-export const OWNER_NUM = "237689360833" // Your number
+export const OWNER_NUM = "2250712668494" // Your number
 
 export const OWNER_TELEGRAM = "@dev_senku" // Your telegram username
 
 export const BOT_NAME = "senku_xmd" // Bot name
 
-export const TELEGRAM_CHANNEL = "@senku_tech_channel" // Your telegram channel name
+export const TELEGRAM_CHANNEL = "none" // Your telegram channel name
 
-export const TELEGRAM_GROUP = "@senku_tech" // Your telegram group name
+export const TELEGRAM_GROUP = "none" // Your telegram group name
 
 export const WA_CHANNEL = "https://whatsapp.com/channel/0029Vb5SsZ49RZAgIU7dkJ0V" //whatsapp channel
 
